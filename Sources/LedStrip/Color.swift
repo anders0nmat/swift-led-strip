c4dabf05f58f8beb98a97802c953105b81ea3e4b
anders0nmat <fullgamefun@gmail.com>
2024-07-20 16:20:52 +0200

    public init for color

--- Sources/LedStrip/Color.swift
+++ Sources/LedStrip/Color.swift
@@ -4,6 +4,12 @@ public struct Color {
 	var green: UInt32
 	var blue: UInt32
 
+	public init(red: UInt32, green: UInt32, blue: UInt32) {
+		self.red = red
+		self.green = green
+		self.blue = blue
+	}
+
 	public static let white = Self(red: 255, green: 255, blue: 255)
 	public static let gray = Self(red: 128, green: 128, blue: 128)
 	public static let black = Self(red: 0, green: 0, blue: 0)
